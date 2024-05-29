73b41b7a689b530b22f1b7262dbef8f952577a2e
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-05-29 12:30:57 +0300

    chore: add global error page

---
+++ frontend/app/error.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import {useRouter} from 'next/navigation'
+import Button from "@/app/ui/custom/Button"
+
+export default function Error() {
+  const router = useRouter();
+  return (
+    <div className="flex flex-col justify-center items-center gap-y-3" style={{height: "60vh"}}>
+      <h1 className="text-red-500 text-2xl font-semibold">Something went wrong!</h1>
+      <div className="flex gap-x-5">
+      <Button body="Go Back" onClick={() => router.back()}/>
+      <Button body="Try Again" onClick={() => router.refresh()} />
+      </div>
+    </div>
+  )
+}
