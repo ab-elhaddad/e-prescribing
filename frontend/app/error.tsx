f52a91d50380f9e0162630c528ec1b65a5063935
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-06-01 12:53:19 +0300

    style (global error page): decrease font weight

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -7,7 +7,7 @@ export default function Error() {
   const router = useRouter();
   return (
     <div className="flex flex-col justify-center items-center gap-y-3" style={{height: "60vh"}}>
-      <h1 className="text-red-500 text-2xl font-semibold">Something went wrong!</h1>
+      <h1 className="text-red-500 text-2xl">Something went wrong!</h1>
       <div className="flex gap-x-5">
       <Button body="Go Back" onClick={() => router.back()}/>
       <Button body="Try Again" onClick={() => router.refresh()} />
