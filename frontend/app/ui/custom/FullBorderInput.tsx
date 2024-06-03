86a459853f33fe4974ebf13e47516b9e2e321bf5
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-06-03 22:52:48 +0300

    refactor: component style

--- frontend/app/ui/custom/FullBorderInput.tsx
+++ frontend/app/ui/custom/FullBorderInput.tsx
@@ -1,4 +1,5 @@
 import { ZodError } from "./ZodError";
+import { clsx } from "clsx";
 
 export default function FullBorderInput({
   type,
@@ -8,6 +9,7 @@ export default function FullBorderInput({
   error,
   defaultValue,
   disabled,
+  label,
 }: {
   type: string;
   placeholder?: string;
@@ -16,18 +18,26 @@ export default function FullBorderInput({
   error?: any;
   defaultValue?: string;
   disabled?: boolean;
+  label?: string;
 }) {
   return (
     <div className="w-full">
+      {label && (
+        <label htmlFor={name} className="text-sm ml-2 text-gray-500 mt-1">
+          {label}
+        </label>
+      )}
       <input
         type={type}
         placeholder={placeholder}
         name={name}
         defaultValue={defaultValue}
         disabled={disabled === undefined ? false : disabled}
-        className={`focus:outline-none text-md px-3 py-2 border rounded-xl bg-white focus:border-sky-500 focus:border-2 my-2 w-80 w-full 
-          ${error ? "border-red-500" : "border-gray-200"}
-          ${disabled ? "bg-gray-200" : "bg-transparent"}`}
+        className={clsx(
+          "focus:outline-none focus:border-sky-500 focus:border-2 text-md px-3 py-2 mb-3 mt-1 border rounded-xl bg-white w-80 w-full",
+          error ? "border-red-500" : "border-gray-200",
+          disabled ? "bg-gray-200" : "bg-transparent"
+        )}
         style={style}
       />
       <ZodError error={error} style={{ width: "fit-content" }} />
