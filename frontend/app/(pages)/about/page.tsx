4c5bae92eea8beb1bcdc72f58def3c47c61ffa37
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-07-07 05:16:32 +0300

    style: make about page friendly with mobile view

--- frontend/app/(pages)/about/page.tsx
+++ frontend/app/(pages)/about/page.tsx
@@ -16,16 +16,16 @@ export default function Page() {
       <div className="flex-col justify-center relative z-30">
         <PageHeader title="About us" />
 
-        <div className="mx-20 flex-col justify-center items-center">
-          <div className="flex justify-around my-12 py-4 backdrop-blur-md bg-white/30 rounded-2xl hover:-translate-y-1 shadow-sm transition duration-500 ease-in-out">
+        <div className="mx-10 md:mx-20 flex-col justify-center items-center">
+          <div className="flex flex-col md:flex-row justify-around my-12 py-4 backdrop-blur-md bg-gray-50 md:bg-white/30 rounded-2xl hover:-translate-y-1 shadow-sm transition duration-500 ease-in-out">
             <div>
               <div className="border-b-4 border-sky-500 w-fit p-3 m-3">
-                <h1 className="text-5xl font-bold text-sky-500">
+                <h1 className="text-4xl md:text-5xl font-bold text-sky-500">
                   Our <span className="text-gray-700">{`doctors'`}</span>
                   <br /> achievements
                 </h1>
               </div>
-              <p className="w-inherit p-3" style={{ width: "50ch" }}>
+              <p className="w-[30ch] md:w-[50ch] p-3 text-sm md:text-base">
                 Our doctors are the best in the world. They have won many awards
                 and have been recognized by many organizations. They are the
                 best in the world. We have variety of doctors who are
@@ -38,16 +38,16 @@ export default function Page() {
             <img
               src="/doctors-discuss.jpg"
               alt="Doctors discuss"
-              className="object-cover h-80 w-auto m-3 rounded-lg hover:-hue-rotate-30 transition duration-500 ease-in-out"
+              className="object-cover w-[72vw] md:w-auto md:h-80 m-3 rounded-lg hover:-hue-rotate-30 transition duration-500 ease-in-out"
             />
           </div>
 
-          <div className="flex flex-col items-center justify-center mt-36">
-            <h1 className="text-5xl font-bold text-gray-700 text-center mb-2">
+          <div className="flex flex-col items-center justify-center mt-24 md:mt-36">
+            <h1 className="text-4xl md:text-5xl font-bold text-gray-700 text-center mb-2">
               Meet our <span className="text-sky-500">doctors</span>
             </h1>
             <Divider size={10} />
-            <div className="mt-8 flex justify-between backdrop-blur-md bg-white/30 rounded-2xl p-3 hover:-translate-y-1 shadow-sm transition duration-500 ease-in-out">
+            <div className="mt-8 flex flex-col md:flex-row justify-between backdrop-blur-md bg-white/30 rounded-2xl p-3 hover:-translate-y-1 shadow-sm transition duration-500 ease-in-out">
               <DoctorCard
                 doctor={{
                   name: "John Doe",
@@ -75,19 +75,19 @@ export default function Page() {
             </div>
           </div>
 
-          <div className="flex justify-around mt-40 my-12 py-4 backdrop-blur-md bg-white/30 rounded-2xl hover:-translate-y-1 shadow-sm transition duration-500 ease-in-out">
+          <div className="flex justify-around mt-20 md:mt-40 my-12 py-4 backdrop-blur-md bg-gray-50 md:bg-white/30 rounded-2xl hover:-translate-y-1 shadow-sm transition duration-500 ease-in-out">
             <img
               src="/medical-survey.jpg"
               alt="Doctors discuss"
-              className="object-cover h-80 w-auto m-3 rounded-lg hover:-hue-rotate-30 transition duration-500 ease-in-out"
+              className="hidden md:block object-cover h-80 w-auto m-3 rounded-lg hover:-hue-rotate-30 transition duration-500 ease-in-out"
             />
             <div>
               <div className="border-b-4 border-sky-500 w-fit p-3 m-3">
-                <h1 className="text-5xl font-bold text-sky-500">
+                <h1 className="text-4xl md:text-5xl font-bold text-sky-500">
                   Why us<span className="text-gray-700">?</span>
                 </h1>
               </div>
-              <p className="w-inherit p-3" style={{ width: "50ch" }}>
+              <p className="w-fit md:w-inherit text-sm md:text-base p-3 w-[30ch] md:w-[50ch]">
                 According to the international health organization, we are the
                 best medical e-service in Egypt in the last 3 years. As
                 E-Prescriping has won the best medical e-service in Egypt in
@@ -124,7 +124,7 @@ function DoctorCard({
           <span className="text-gray-800">Dr. </span>
           {doctor.name}
         </h1>
-        <p className="text-sm" style={{ width: "30ch" }}>
+        <p className="text-xs md:text-sm w-[20ch] md:w-[30ch]">
           {doctor.description}
         </p>
       </div>
