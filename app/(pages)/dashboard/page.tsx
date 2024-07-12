7e7c381a256670ca7f3bbba4fe67b87d035b9f18
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-07-12 13:15:08 +0300

    chore: remove useless page

--- app/(pages)/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-
-export default function Page() {
-  const userType = cookies().get("userType")?.value;
-  redirect(`/dashboard/${userType}`);
-}
