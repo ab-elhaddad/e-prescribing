dc1c593ffaa81d1b1d7be6e64a959d3d632ca474
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-07-18 14:04:02 +0300

    refactor: remove useless files

--- app/lib/constants.ts
+++
@@ -1,5 +0,0 @@
-export const typeShorten: Record<string, 'doc' | 'ast' | 'pat'> = {
-  doctor: "doc",
-  assistant: "ast",
-  patient: "pat",
-};
--- app/lib/utils/decodeJwt.ts
+++
@@ -1,3 +0,0 @@
-export default function decodeJwt(token: string): Object {
-  return JSON.parse(window.atob(token.split(".")[1]));
-}
