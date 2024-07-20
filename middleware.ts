a75371586d0150af7bce57814806755cc4bf20af
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-07-20 13:10:19 +0300

    chore: assign user as patient if their role not assigned

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,34 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {
+  clerkClient,
+  clerkMiddleware,
+  ClerkMiddlewareAuth,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
+  setRole(auth);
   if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+async function setRole(auth: ClerkMiddlewareAuth) {
+  const userId = auth().userId;
+  if (!userId) return;
+
+  const user = await clerkClient().users.getUser(userId);
+  if (!user) return;
+
+  if (user.publicMetadata.role === undefined) {
+    await clerkClient().users.updateUser(userId, {
+      publicMetadata: {
+        ...user.publicMetadata,
+        role: "patient",
+      },
+    });
+  }
+}
