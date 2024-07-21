aca612c98a6cd1bc26d18091ce74fa7bda7e1507
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-07-21 09:15:55 +0300

    fix: return type

--- app/actions/doctor/updatePrescriptionAction.ts
+++ app/actions/doctor/updatePrescriptionAction.ts
@@ -1,5 +1,9 @@
-export async function updatePrescription(
+import { ActionReturn } from "../types";
+
+export async function updatePrescriptionAction(
   drugs: string[],
   prevState: any,
   formData: FormData,
-) {}
+): Promise<ActionReturn<{ server?: string }>> {
+  return { success: true };
+}
