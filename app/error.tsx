c39e827fccbec2fdf7e1447740e2f9ce7bdacb55
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-07-21 08:15:48 +0300

    style: format code

--- app/error.tsx
+++ app/error.tsx
@@ -1,17 +1,20 @@
-"use client"
+"use client";
 
-import {useRouter} from 'next/navigation'
-import Button from "@/components/inputs/Button"
+import { useRouter } from "next/navigation";
+import Button from "@/components/inputs/Button";
 
 export default function Error() {
   const router = useRouter();
   return (
-    <div className="flex flex-col justify-center items-center gap-y-3" style={{height: "60vh"}}>
-      <h1 className="text-red-500 text-2xl">Something went wrong!</h1>
+    <div
+      className="flex flex-col items-center justify-center gap-y-3"
+      style={{ height: "60vh" }}
+    >
+      <h1 className="text-2xl text-red-500">Something went wrong!</h1>
       <div className="flex gap-x-5">
-      <Button body="Go Back" onClick={() => router.back()}/>
-      <Button body="Try Again" onClick={() => router.refresh()} />
+        <Button body="Go Back" onClick={() => router.back()} />
+        <Button body="Try Again" onClick={() => router.refresh()} />
       </div>
     </div>
-  )
+  );
 }
