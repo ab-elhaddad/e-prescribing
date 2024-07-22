47ab223f13682ad77ead2be4e5ea5d1003a5df8a
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-07-22 18:51:30 +0300

    feat: add OCR env vars config

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,6 +1,8 @@
 const config = {
   mongoUri: process.env.MONGO_URI,
-  mongoDb: process.env.MONGO_DB,
+  mongoDb: process.env.MONGO_DB || "test",
+  ocrApi: process.env.OCR_API,
+  ocrApiKey: process.env.OCR_API_KEY,
 };
 
 export default config;
